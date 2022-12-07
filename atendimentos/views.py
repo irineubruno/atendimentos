79868d87dae2dee75cdb12defb01dfeb02e1144e
Bruno Irineu <irineubruno@hotmail.com>
2022-12-07 00:07:26 -0400

    v.2

--- atendimentos/views.py
+++ atendimentos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import  CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Empresa, Cliente, Tecnico, Tipo_departamento, Tipo_atendimento, ordem_servico
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
